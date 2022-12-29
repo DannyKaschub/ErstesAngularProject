09a4c089ae66850f6f3db4271259987eb1e36938
DannyKaschub <dannykashub98@gmail.com>
2022-12-29 02:10:37 +0100

    add pics and start slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'WebsiteMitAngular';
+export class AppComponent implements OnInit {
+  images = ['ets2_20210524_235109_00.png', 'ets2_20221228_033223_00.png', 'ets2_20221228_033311_00.png'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 1)
+
+    }, 8000);
+
+  }
 }
